ba3fc7999b4626268763bd26b9978ec699ffc645
DebFullstack <debbieking1979@gmail.com>
2023-07-31 20:10:57 -0400

    all functionality works

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
-import Authenticate from "./components/Authenticate";
 import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
+import { useState } from "react";
+
+
 export default function App() {
- 
+  const [token, setToken] = useState(null);
   return (
     <>
-      <Authenticate />
-      <SignUpForm />
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
     </>
   );
 }
--- reactApp/src/components/SignUpForm.jsx
+++ reactApp/src/components/SignUpForm.jsx
@@ -1,10 +1,67 @@
-import { useState } from 'react';
+import { useState } from "react";
 
+export default function SignUpForm(props) {
+  const { setToken } = props;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [signedUpUsername, setSignedUpUsername] = useState(null);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-export default function SignUpForm() {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [error, setError] = useState(null);
-	return <h2>Sign Up!</h2>;
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long.");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      const result = await response.json();
+      setToken(result.token);
+      setSignedUpUsername(username);
+
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
   }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      {signedUpUsername && (
+        <p>Thank you for signing up, {signedUpUsername}!</p>
+      )}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
